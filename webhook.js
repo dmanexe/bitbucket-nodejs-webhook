c21cc71817a1ccb92f858d133f127b6f25c79f81
Daniel Birlem <dman.exe@gmail.com>
2019-03-01 15:21:14 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,15 +1,18 @@
 const repo = "/path/to/your/repo";
 
 const http = require('http');
-const crypto = require('crypto');
+const crypto = require('crypto'); // in case you need to use SSL/TLS/HTTPS
 const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
     req.on('data', function(chunk) {
-        if (req.headers['x-hook-uuid'] == 'xxxxx') { // Find this in your webhook settings
-            exec('cd ' + repo + ' && git pull origin master');
+        if (req.headers['x-hook-uuid'] == 'xxxxx') { 
+        // Find this in your webhook settings in BitBucket -> Settings -> Webhooks
+            exec('cd ' + repo + ' && git pull origin master'); 
+            // change origin and master if these aren't your git repo names
         }
+        // forgoeing any sort of error message here to discourage random access        
     });
 
     res.end();
-}).listen(64554); // change port or use ufw to allow
+}).listen(xxxxx); // change to desired port and configure your firewall to allow TCP connections on this incoming port
